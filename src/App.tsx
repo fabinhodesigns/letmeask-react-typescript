65cf2f433c565d2d198eedebf1ebd3ea7862f2b7
fabinho070 <freitasfabiano08@gmail.com>
2022-03-05 15:21:10 -0300

    Adicionando ajustes de layout e criação de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Button } from './components/Button'
+import { Home } from './pages/Home' 
 
 function App() {
   return (
-    <Button/>
+    <Home/>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import logoGoogle from '../assets/images/google-icon.svg';
+
+import '../styles/auth.scss';
+
+export function Home() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas." />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button>
+            <img src={logoGoogle} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div>ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <button type="submit">
+              Entrar na sala</button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
