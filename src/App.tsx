d0e49dea1491e29476315dfaa2b8cecfc2eb42f8
fabinho070 <freitasfabiano08@gmail.com>
2022-03-10 23:51:17 -0300

    Ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-// import { Home } from './pages/Home' 
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
 
 function App() {
   return (
-    // <Home/>
-    <NewRoom />
+    <BrowserRouter>
+      <Route path="/"><Home /></Route>
+      <Route path="/rooms/new"><NewRoom /></Route>
+    </BrowserRouter>
   );
 }
 
