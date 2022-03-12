018bbba0d414f27f846f2dcfb8c1ea1f8e9e54e5
fabinho070 <freitasfabiano08@gmail.com>
2022-03-12 08:56:48 -0300

    Ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import { Home } from './pages/Home'
-import { NewRoom } from './pages/NewRoom'
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/"><Home /></Route>
-      <Route path="/rooms/new"><NewRoom /></Route>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
     </BrowserRouter>
   );
 }
